76d894b737019ed465b4e89a9896d506b1dcd124
MalakaSupun <malakag@sltc.ac.lk>
2024-12-31 22:02:57 +0530

    Updating files

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
-print("Malaka")
+import os
+import json
+
+import streamlit as st
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_groq import ChatGroq
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+print("done")
