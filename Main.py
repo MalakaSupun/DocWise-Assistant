4839e8e04a466cbc68865ae57d2f83cd39a29c77
MalakaSupun <malakag@sltc.ac.lk>
2025-01-02 00:43:13 +0530

    Update files

--- Main.py
+++ Main.py
@@ -23,3 +23,62 @@ def setup_vectorstore():
                          embedding_function=embeddings)
     return vectorstore
 
+
+def chat_chain(vectorstore):
+    llm = ChatGroq(model="llama-3.1-70b-versatile",
+                   temperature=0)
+    retriever = vectorstore.as_retriever()
+    memory = ConversationBufferMemory(
+        llm=llm,
+        output_key="answer",
+        memory_key="chat_history",
+        return_messages=True
+    )
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        chain_type="stuff",
+        memory=memory,
+        verbose=True,
+        return_source_documents=True
+    )
+
+    return chain
+
+
+st.set_page_config(
+    page_title="Multi Doc Chat",
+    page_icon = "📚",
+    layout="centered"
+)
+
+st.title("📚 Multi Documents Chatbot")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+if "vectorstore" not in st.session_state:
+    st.session_state.vectorstore = setup_vectorstore()
+
+if "conversationsal_chain" not in st.session_state:
+    st.session_state.conversationsal_chain = chat_chain(st.session_state.vectorstore)
+
+
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+user_input = st.chat_input("Ask AI...")
+
+if user_input:
+    st.session_state.chat_history.append({"role": "user", "content": user_input})
+
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+
+    with st.chat_message("assistant"):
+        response = st.session_state.conversationsal_chain({"question": user_input})
+        assistant_response = response["answer"]
+        st.markdown(assistant_response)
+        st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
